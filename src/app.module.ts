71aefbc52446eab3e77dd2787637ff2da292c723
Azimkhan93 <aabdulsatarov@gmail.com>
2025-10-31 00:30:52 +0500

    [LC-44] create crud for colors

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ColorsModule } from './colors/colors.module';
 
 @Module({
-  imports: [CustomersModule, PrismaModule],
+  imports: [CustomersModule, PrismaModule, ColorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
