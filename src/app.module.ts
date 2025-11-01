c8177d529f4842412a255391e3faadb6dbaee66e
Azimkhan93 <aabdulsatarov@gmail.com>
2025-11-02 00:07:29 +0500

    [LC-46] create crud for categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [CustomersModule, PrismaModule],
+  imports: [CustomersModule, PrismaModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
